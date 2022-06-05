f04dbde215b107ea42fb80f03ae9ac02c8cccb1a
pedrosnow <pedro6489@gmail.com>
2022-06-05 10:38:23 -0300

    fist commit

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
-const express = require('express')
-const app = express()
-const engine = require('express-handlebars');
-const bodyParser = require('body-parser')
+const express = require("express");
 
+const app = express();
 
-//  handlebars
-
-const handlebars = engine.create({defaultLayout: 'main'})
-app.engine('handlebars', handlebars.engine);
-
-app.set('view engine', 'handlebars');
-app.set('views', './App/views');
-
-
-// Body parde
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-// Rotas
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.post('/salvar', (req, res)=>{
-  res.send(req.body.nome)
-})
-
+app.get('/', (req, res)=>{
+    res.send('wello')
+});
 
 
 app.listen(3000)
-
